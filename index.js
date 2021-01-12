186f2acac57099bf7b6f8f224ad94f41bd1f3ea4
gkcksrbs <70510732+gkcksrbs@users.noreply.github.com>
2021-01-12 22:38:56 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gkcksrbs:hcg1208*@boiler-plate.c7vyp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -11,7 +21,21 @@ mongoose.connect('mongodb+srv://gkcksrbs:hcg1208*@boiler-plate.c7vyp.mongodb.net
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, 안녕하세요!')
+  res.send('Hello World!, 안녕하세요! 좋은 하루 되세요!')
+})
+
+app.post('/register', (req, res) => {
+  //회원가입 할 때 필요한 정보들을 client에서 가져오면 
+  //그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
